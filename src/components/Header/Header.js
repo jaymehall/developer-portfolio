baf04849d82b236719be6ffe96480b1c92a957c4
Pro-Code-art <jayme.coder@gmail.com>
2021-03-22 22:09:15 -0700

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Header() {
+    return (
+        <div className="seatedImg">
+            <div className="headerText">
+                <h1 className="helloWorld">Hello World. I'm Jayme, and I carve out corners of the internet where dreams come true. One step at a time.</h1>
+                
+            </div>
+            {/* <img className="seated-img" alt="business woman seated on a blue bench outside" src="./images/seated-soft.jpg"/> */}
+        </div>
+    )
+}
+
+export default Header;
