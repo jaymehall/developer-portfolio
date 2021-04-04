42a393b8a74f56993f04c547c3c195edd7eb61c8
Pro-Code-art <jayme.coder@gmail.com>
2021-04-04 13:53:34 -0700

    updated technologies content

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -9,9 +9,11 @@ function Boxes(props) {
         <Card.Body>
           <Card.Title>{props.heading}</Card.Title>
           <Card.Text>
-                <p>{props.child1}</p>
-                <p>{props.child2}</p>
-                <p>{props.child3}</p>
+            <p>{props.child1}</p>
+            <p>{props.child2}</p>
+            <p>{props.child3}</p>
+            <p>{props.child4}</p>
+            <p>{props.child5}</p>
           </Card.Text>
         </Card.Body>
       </Card>
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -13,23 +13,32 @@ function Technologies () {
                 child1="HTML5"
                 child2="CSS3 | SASS"
                 child3="JavaScript"
+                child4="Bootstrap | Tailwind CSS"
+                child5="React | Context API"
                 />
                 <Boxes className="techBox"
                 heading="Back End"
                 child1="Node JS"
                 child2="Express"
                 child3="MySQL"
+                child4="MongoDB"
+                child5="Handlebars.js"
                 />
                 <Boxes className="techBox"
                 heading="Deployment"
-                child1="Heroku"
-                child2="Netlify"
-                child3="Gh Pages"/>
+                child1="Gulp"
+                child2="Heroku"
+                child3="Netlify"
+                child4="Gh Pages"
+                child5="DNN .NET CMS"
+                />
                 <Boxes className="techBox"
                 heading="Tools"
                 child1="Git"
-                child2="Photoshop"
-                child3="Kanban"/>
+                child2="Kanban"
+                child3="Photoshop | Procreate"
+                child4="Firebase | Passport"
+                child5="Salesforce | CMS"/>
             </div>
         </div>
     )
