7d61fa4b24f899058944d5b8e446c3714f61ec55
Pro-Code-art <jayme.coder@gmail.com>
2021-04-04 13:01:20 -0700

    edited card component and adjusted technologies section style to display as flex

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,50 +1,22 @@
 import React from "react";
 import { CardColumns, Card } from "react-bootstrap";
+import "./index.css";
 
 function Boxes(props) {
   return (
     <CardColumns>
-
-      <Card className="text-center">
+      <Card className="text-center card">
         <Card.Body>
-          <Card.Title>{props.heading1}</Card.Title>
+          <Card.Title>{props.heading}</Card.Title>
           <Card.Text>
             <ul>
                 <li>{props.child1}</li>
                 <li>{props.child2}</li>
                 <li>{props.child3}</li>
-                
             </ul>
           </Card.Text>
         </Card.Body>
       </Card>
-      <Card className="text-center">
-        <Card.Body>
-          <Card.Title>{props.heading2}</Card.Title>
-          <Card.Text>
-            <ul>
-                <li>{props.child4}</li>
-                <li>{props.child5}</li>
-                <li>{props.child6}</li>
-                
-            </ul>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="text-center">
-        <Card.Body>
-          <Card.Title>{props.heading3}</Card.Title>
-          <Card.Text>
-            <ul>
-                <li>{props.child7}</li>
-                <li>{props.child8}</li>
-                <li>{props.child9}</li>
-                
-            </ul>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    
     </CardColumns>
   );
 }
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -8,20 +8,28 @@ function Technologies () {
         <div className="middleBg">
             <div className="film"></div>
             <div className="wrapper">
-                <Boxes
-                heading1="Front end"
+                <Boxes className="techBox"
+                heading="Front end"
                 child1="HTML5"
                 child2="CSS3 | SASS"
                 child3="JavaScript"
-                heading2="Back End"
-                child4="Node JS"
-                child5="Express"
-                child6="MySQL"
-                heading3="Deployment"
-                child7="Heroku"
-                child8="Netlify"
-                child9="GitHub"
                 />
+                <Boxes className="techBox"
+                heading="Back End"
+                child1="Node JS"
+                child2="Express"
+                child3="MySQL"
+                />
+                <Boxes className="techBox"
+                heading="Deployment"
+                child1="Heroku"
+                child2="Netlify"
+                child3="Gh Pages"/>
+                <Boxes className="techBox"
+                heading="Tools"
+                child1="Git"
+                child2="Photoshop"
+                child3="Kanban"/>
             </div>
         </div>
     )
