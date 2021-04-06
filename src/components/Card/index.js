aa2658cf44ad0ff91f2384c4f3edc7cf68688d87
Pro-Code-art <jayme.coder@gmail.com>
2021-04-05 21:52:18 -0700

    added animations and styling to technologies and project section; also created new flip card component to accomplish this

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,25 +1,26 @@
 import React from "react";
 import { CardColumns, Card } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 import "./index.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Box(props) {
   return (
-    <CardColumns>
-      <Card className="text-center card">
-        <Card.Body>
-        <FontAwesomeIcon className="icon" icon={props.icon} />
-          <Card.Title className="cardTitle">{props.heading}</Card.Title>
-          <Card.Text>
-            <p>{props.child1}</p>
-            <p>{props.child2}</p>
-            <p>{props.child3}</p>
-            <p>{props.child4}</p>
-            <p>{props.child5}</p>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </CardColumns>
+          <CardColumns>
+            <Card className="text-center card">
+              <Card.Body>
+                <FontAwesomeIcon className="icon" icon={props.icon} />
+                <Card.Title className="cardTitle">{props.heading}</Card.Title>
+                <Card.Text>
+                  <p>{props.child1}</p>
+                  <p>{props.child2}</p>
+                  <p>{props.child3}</p>
+                  <p>{props.child4}</p>
+                  <p>{props.child5}</p>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </CardColumns>
   );
 }
 
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,47 +1,54 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Box from "../Card";
-import "./projects.css"
+import "./projects.css";
+import FlipCard from "../FlipCard/FlipCard";
 
-function Projects () {
-    return (
-        <div>
-            <h2 className="projectsTitle">Projects</h2>
-            <div className="projectsContainer">
-                <Box className="projectCard"
-                heading="Crypto Tracker"
-                child1="JavaScript"
-                child2="Passport Authentication"
-                child3="Tailwind CSS"
-                child4="Node JS | Express"
-                child5="Handlebars.js"
-                />
-                <Box className="projectCard"
-                heading="Nom That Burger"
-                child1="Node JS"
-                child2="Express"
-                child3="MySQL"
-                child4="MongoDB"
-                child5="Handlebars.js"
-                />
-                <Box className="projectCard"
-                heading="Weather Dash"
-                child1="HTML 5 |CSS3"
-                child2= "Bootstrap"
-                child3="JavaScript | jQuery"
-                child4="OpenWeather API"
-                child5="AJAX"
-                />
-                <Box className="projectCard"
-                heading="GoTeam"
-                child1="React"
-                child2="JavaScript"
-                child3="Bootstrap"
-                child4="CSS3"
-                child5="SASS"/>
-            </div>
-        </div>
-    )
+function Projects() {
+  return (
+    <div>
+      <h2 className="projectsTitle">Projects</h2>
+      <div className="projectsContainer">
+        <FlipCard
+          className="projectCard"
+          heading="Crypto Tracker"
+          child1="JavaScript"
+          child2="Passport Authentication"
+          child3="Tailwind CSS"
+          child4="Node JS | Express"
+          child5="Handlebars.js"
+        />
+
+        <FlipCard
+          className="projectCard"
+          heading="Nom That Burger"
+          child1="Node JS"
+          child2="Express"
+          child3="MySQL"
+          child4="MongoDB"
+          child5="Handlebars.js"
+        />
+        <FlipCard
+          className="projectCard"
+          heading="Weather Dash"
+          child1="HTML 5 |CSS3"
+          child2="Bootstrap"
+          child3="JavaScript | jQuery"
+          child4="OpenWeather API"
+          child5="AJAX"
+        />
+        <FlipCard
+          className="projectCard"
+          heading="GoTeam"
+          child1="React"
+          child2="JavaScript"
+          child3="Bootstrap"
+          child4="CSS3"
+          child5="SASS"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
