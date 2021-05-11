d69c48f359d6bf454808fca96d172d1fc55fcf94
Pro-Code-art <jayme.coder@gmail.com>
2021-05-10 21:25:35 -0700

    edited technolgies list on project section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -44,12 +44,12 @@ function Projects({ gitRepURL }) {
           setShowProject={setShowProject}
           className="projectCard"
           heading="Crypto Tracker"
-          child1="JavaScript"
+          child1="JavaScript | MySQL"
           child2="Passport Authentication"
-          child3="Tailwind CSS"
+          child3="Tailwind CSS | CSS3"
           child4="Node JS | Express"
           child5="Handlebars.js"
-          description="Allows user to sign up, log in, search and save various crypto coins to a MySQL database based on the current price and trading indicators."
+          description="Full-Stack app that allows user to sign up, log in, search and save various crypto coins to a MySQL database based on the current price and trading indicators."
           gitRepURL="https://github.com/team-crypto-currency/crypto-investment-tracker"
         />
 
@@ -57,10 +57,10 @@ function Projects({ gitRepURL }) {
           setShowProject={setShowProject}
           className="projectCard"
           heading="Nom That Burger"
-          child1="Node JS"
-          child2="Express"
+          child1="JavaScript"
+          child2="Node JS | Express"
           child3="MySQL"
-          child4="MongoDB"
+          child4="Bootstrap | CSS3"
           child5="Handlebars.js"
           description="Allows the user to type in and save their favorite burgers to a MySQL database. User can then devour burgers at will!"
         />
