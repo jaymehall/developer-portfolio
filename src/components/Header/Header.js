edbf3a64fc77d4f0867bacd25a9d5b719aaf590a
Jayme <jlm_developer@protonmail.com>
2022-05-14 13:35:40 -0500

    changed backgrounds and stylings

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,20 +3,22 @@ import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { faAtom } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
+import logo  from "./theprivacydevlogo.png";
 
 function Header() {
   return (
     <div className="bg">
       {/* <div className="layer"></div> */}
       <div className="logo">
-        <FontAwesomeIcon className="iconLogo" icon={faAtom} />
-        <p>Source Logic</p>
+        {/* <img src={logo} height="10%" width="10%" /> */}
+        {/* <FontAwesomeIcon className="iconLogo" icon={faAtom} />
+        <p>Source Logic</p> */}
       </div>
       <div className="headerText">
-        <h1 className="name">The Privacy Dev</h1>
-        <p>Software Developer</p>
+        {/* <h1 className="name">The Privacy Dev</h1> */}
+        <h4 style={{padding: "20px", marginTop: "50px"}}>Software Developer</h4>
         <Router>
           <Button className="viewWorkBtn" variant="light">
             <HashLink className="viewWork" to="/#viewWork">
