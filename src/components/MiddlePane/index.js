b18fd8aec9ed6afbcd885a9efca59e0d598dfbcf
The Privacy Dev <theprivacydev@protonmail.com>
2024-06-09 20:11:13 -0500

    adjusted wording in bio

--- src/components/MiddlePane/index.js
+++ src/components/MiddlePane/index.js
@@ -30,7 +30,7 @@ function MiddlePane() {
           <article>
             <h5 className="sectionTitle">Time-Tested Results</h5>
             <div className="introText">
-              As an experienced full stack software developer, I use a
+              As an experienced full-stack software engineer, I use a
               consistent approach to quickly produce highly manageable, clean code with a focus on accessibility and scalability. I enjoy using my experience to mentor more junior developers and bridge the gap between business requirements and the research and development team.
             </div>
           </article>
