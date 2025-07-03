71ff34321cb8ef7be5d016c65ba6bee418c6031d
theprivacydev <theprivacydev@protonmail.com.noreply.github.com>
2025-07-03 17:02:31 -0500

    changed profile pic

--- src/components/MiddlePane/index.js
+++ src/components/MiddlePane/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import MyPic from "./MySmallPic.jpeg";
+import MyPic from "./profile.jpg";
 
 function MiddlePane() {
   return (
